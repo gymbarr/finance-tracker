28aed64da5f4593152e745f7397f805229c86bf8
Andrey Timakhovich <stewart96.at@gmail.com>
2022-07-22 12:32:49 +0300

    create and display stock objects in browser

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -7,7 +7,8 @@ class Stock < ApplicationRecord
       endpoint: 'https://sandbox.iexapis.com/v1'
     )
 
-    client.price(ticker_symbol)
+    # return Stock object
+    new(ticker: ticker_symbol, name: client.company(ticker_symbol).company_name, last_price: client.price(ticker_symbol))
   end
 
 end
