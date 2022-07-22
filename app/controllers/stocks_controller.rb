73d331d5d4670f135717cc0ec236d8d09e239c84
Andrey Timakhovich <stewart96.at@gmail.com>
2022-07-22 13:03:24 +0300

    add flash messages to invalid stock search

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -1,6 +1,21 @@
 class StocksController < ApplicationController
   def search
-    @stock = Stock.new_lookup(params[:stock])
-    render 'users/my_portfolio'
+    # if search input is not empty
+    if params[:stock].present?
+      # get the Stock object with all params
+      @stock = Stock.new_lookup(params[:stock])
+      if !@stock.nil?
+        # reload current page
+        render 'users/my_portfolio'
+      else
+        # if symbol wasn't found display alert message
+        flash[:alert] = "Symbol not found"
+        redirect_to my_portfolio_path
+      end
+    else
+      # display alert message
+      flash[:alert] = "Enter a symbol to search"
+      redirect_to my_portfolio_path
+    end
   end
 end
--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,14 +1,19 @@
 class Stock < ApplicationRecord
 
   def self.new_lookup(ticker_symbol)
+    # connect to the IEX API
     client = IEX::Api::Client.new(
       publishable_token: Rails.application.credentials.iex_client[:sandbox_publish_key],
       secret_token: Rails.application.credentials.iex_client[:sandbox_secret_key],
       endpoint: 'https://sandbox.iexapis.com/v1'
     )
-
-    # return Stock object
-    new(ticker: ticker_symbol, name: client.company(ticker_symbol).company_name, last_price: client.price(ticker_symbol))
+    
+    begin
+    # return Stock object in success scenario
+      new(ticker: ticker_symbol, name: client.company(ticker_symbol).company_name, last_price: client.price(ticker_symbol))
+    rescue => exception
+      # if the exception was thrown return nil
+      nil
+    end
   end
-
 end
