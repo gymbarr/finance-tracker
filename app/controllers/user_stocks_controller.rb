b597d5ddc3a4820e32f5922afcfcc3c375b896c0
Andrey Timakhovich <stewart96.at@gmail.com>
2022-07-25 18:10:01 +0300

    add stocks tracking feature

--- app/controllers/user_stocks_controller.rb
+++ app/controllers/user_stocks_controller.rb
@@ -1,2 +1,18 @@
 class UserStocksController < ApplicationController
+
+  def create
+    # check if entered stock is already exist in db
+    stock = Stock.check_db(params[:ticker])
+
+    # if not exist in db then connect to the API, retreive requested stock and save it to db
+    if stock.blank?
+      stock = Stock.new_lookup(params[:ticker])
+      stock.save
+    end
+
+    # create relation in the user_stock table
+    @user_stock = UserStock.create(user: current_user, stock: stock)
+    flash[:notice] = "Stock #{stock.name} was successfully added to your portfolio"
+    redirect_to my_portfolio_path
+  end
 end
