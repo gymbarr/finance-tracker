4b89946d65703c5abba7e09f8de8672800261848
Andrey Timakhovich <stewart96.at@gmail.com>
2022-07-26 16:16:36 +0300

    accept additional fields - sign up

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,8 +4,10 @@ class ApplicationController < ActionController::Base
 
   protected
 
+  # add additional fields for user profile (see devise gem documentation)
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name])
   end
 
 end
