61082822e3fcf4500f186acbfa3ca7cc43018cdb
Andrey Timakhovich <stewart96.at@gmail.com>
2022-08-10 17:48:28 +0300

    create nav_tab helper method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+  def nav_tab(title, url, options = {})
+    css_class = request.path == url ? 'active' : ''
+
+    options[:class] = if options[:class]
+                        options[:class] + ' ' + css_class
+                      else
+                        css_class
+                      end
+    
+    link_to title, url, options
+  end
 end
